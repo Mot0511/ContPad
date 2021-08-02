0f0cb3b216fb888641837699b7722bd93f025a8a
Mot0511 <suvorov.matvej9@gmail.com>
2021-08-02 11:05:53 +0300

    added reg/login

--- main.py
+++ main.py
@@ -8,28 +8,32 @@ app = Flask(__name__)
 def index(name=None):
     return render_template('index.html', error='')
 
-@app.route("/account", methods=['GET', 'POST'])
-def account():
+@app.route('/reg', methods=['GET', 'POST'])
+def reg():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
 
-        with sq.connect("db.db") as con:
+        with sq.connect('db.db') as con:
+            cur = con.cursor()
+            cur.execute(f"INSERT INTO users VALUES (?, ?)", (login, password))
+            return render_template('account.html', login=login)
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('password')
+        with sq.connect('db.db') as con:
             cur = con.cursor()
             get = cur.execute(f"SELECT login, password FROM users WHERE login == '{login}'")
-            print(get.fetchone())
-            if get.fetchone() is None:
-                cur.execute(f"INSERT INTO users VALUES (?, ?)", (login, password))
-                print(get.fetchone())
+            if get.fetchone()[1] == password:
                 return render_template('account.html', login=login)
+
             else:
-                if get.fetchone()[2] == password:
-                    print(get.fetchone())
-                    return render_template('account.html', login=login)
-
-                else:
-                    return render_template('index.html', error='No password')
-
+                return render_template('index.html', error='No password')
 
 @app.route("/news")
 def news(name=None):
