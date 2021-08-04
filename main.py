e191d4133806a80d0b0256b0270efbab545b041a
Mot0511 <suvorov.matvej9@gmail.com>
2021-08-04 17:13:03 +0400

    _

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import *
 import sqlite3 as sq
+import random
 
 app = Flask(__name__)
 
@@ -38,5 +39,45 @@ def login():
 @app.route("/news")
 def news(name=None):
     return render_template('news.html', name=name)
+
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'tmp'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        login = request.form.get('login')
+        img = 'img'
+        
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(str(random.randint(0, 100000)) + '.png')
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with sq.connect('db.db') as con:
+                cur = con.cursor()
+                get = cur.execute("INSERT INTO posts VALUES (?, ? ,?)", (login, name, img))
+                return render_template('account.html', login=login)
+    
 
 app.run()
